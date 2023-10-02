fa9d4a68864c06d2f7ff3ba7180eb5461face5c2
Sjaak Schilperoort <sjaak@xs4all.nl>
2023-10-02 15:19:44 -0400

    Use IconProps for IconButton

--- src/components/__tests__/IconButton.test.tsx
+++ src/components/__tests__/IconButton.test.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import { render, fireEvent } from '@testing-library/react-native'
 
+import { IconProps } from '../Icon'
 import IconButton from '../IconButton'
 
 describe('IconButton', () => {
+  const icon: IconProps = { name: 'chevron-left', size: 20, color: 'black' }
   test('Rendering', () => {
     // GIVEN
-    const { toJSON } = render(
-      <IconButton containerStyle={{ flex: 1 }} disabled={false} iconName="chevron-left" size={20} color="black" />,
-    )
+    const { toJSON } = render(<IconButton containerStyle={{ flex: 1 }} disabled={false} icon={icon} />)
 
     // THEN
     expect(toJSON()).toMatchSnapshot()
@@ -18,9 +18,7 @@ describe('IconButton', () => {
   test('Handle press when enabled', () => {
     // GIVEN
     const mockOnPress = jest.fn()
-    const { getByTestId } = render(
-      <IconButton disabled={false} onPress={mockOnPress} iconName="chevron-left" size={20} color="black" />,
-    )
+    const { getByTestId } = render(<IconButton disabled={false} onPress={mockOnPress} icon={icon} />)
 
     // WHEN
     const pressable = getByTestId('pressable')
@@ -33,9 +31,7 @@ describe('IconButton', () => {
   test('Do not handle press when disabled', () => {
     // GIVEN
     const mockOnPress = jest.fn()
-    const { getByTestId } = render(
-      <IconButton disabled onPress={mockOnPress} iconName="chevron-left" size={20} color="black" />,
-    )
+    const { getByTestId } = render(<IconButton disabled onPress={mockOnPress} icon={icon} />)
 
     // WHEN
     const pressable = getByTestId('pressable')
