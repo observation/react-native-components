1962635031f05d5076bfadf699cff279c1a7b3f4
Sjaak Schilperoort <sjaak@xs4all.nl>
2026-01-15 09:56:33 +0100

    Replace deprecated method InteractionManager.runAfterInteractions

--- jest.mocks.js
+++ jest.mocks.js
@@ -2,32 +2,58 @@
 import React from 'react'
 import { Image } from 'react-native'
 
-// Mock font awesome
-const Icon = 'Icon'
-const getIconType = (prefix) => {
-  switch (prefix) {
-    case 'fas':
-      return 'solid'
-    case 'fal':
-      return 'light'
-    case 'fab':
-      return 'brand'
-    default:
-      throw new Error()
+// === Globals ===
+
+if (typeof global.requestIdleCallback === 'undefined') {
+  global.requestIdleCallback = function (callback) {
+    return setTimeout(function () {
+      callback({
+        timeRemaining: function () {
+          return 50
+        },
+        didTimeout: false,
+      })
+    }, 0)
   }
 }
-jest.mock('@fortawesome/react-native-fontawesome', () => ({
-  FontAwesomeIcon: (faIcon) => (
-    <Icon
-      testID={faIcon.testID}
-      color={faIcon.color}
-      name={faIcon.icon.iconName}
-      size={faIcon.size}
-      style={faIcon.style}
-      type={getIconType(faIcon.icon.prefix)}
-      transform={faIcon.transform}
-    />
-  ),
-}))
+
+if (typeof global.cancelIdleCallback === 'undefined') {
+  global.cancelIdleCallback = function (id) {
+    clearTimeout(id)
+  }
+}
+
+// === Third-party libraries ===
+
+jest.mock('@fortawesome/react-native-fontawesome', () => {
+  const Icon = 'Icon'
+
+  const getIconType = (prefix) => {
+    switch (prefix) {
+      case 'fas':
+        return 'solid'
+      case 'fal':
+        return 'light'
+      case 'fab':
+        return 'brand'
+      default:
+        throw new Error()
+    }
+  }
+
+  return {
+    FontAwesomeIcon: (faIcon) => (
+      <Icon
+        testID={faIcon.testID}
+        color={faIcon.color}
+        name={faIcon.icon.iconName}
+        size={faIcon.size}
+        style={faIcon.style}
+        type={getIconType(faIcon.icon.prefix)}
+        transform={faIcon.transform}
+      />
+    ),
+  }
+})
 
 Image.getSizeWithHeaders = jest.fn(() => Promise.resolve({ width: 0, height: 0 }))
